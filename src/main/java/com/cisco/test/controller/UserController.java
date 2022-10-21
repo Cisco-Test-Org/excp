8be572c6b82e1f13c697ab84acfa54ce704a421f
sovisrushain <sovisrushain@gmail.com>
2022-10-21 11:48:58 +0530

    init

---
+++ src/main/java/com/cisco/test/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.cisco.test.controller;
+
+import com.cisco.test.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final List<User> users = new ArrayList<>();
+
+    @PostMapping
+    public void saveUser(@RequestBody User user) {
+        //UserValidation.validateUser(user);
+        users.add(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return users;
+    }
+}
---
+++ src/main/java/com/cisco/test/util/UserValidation.java
@@ -0,0 +1,9 @@
+package com.cisco.test.util;
+
+import com.cisco.test.dto.User;
+
+public class UserValidation {
+    public static void validateUser(User user) {
+
+    }
+}
